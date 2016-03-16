39de52a433c85bfc0d76a15c68bd7f4319ac8814
Valerie Sharp <Valeriesharp93@gmail.com>
2016-03-16 15:39:05 -0700

    Some changes

--- resources/views/user/setting.blade.php
+++ resources/views/user/setting.blade.php
@@ -3,87 +3,87 @@
 
 <div class="container">
 
-<h2 class="login--welcome">Settings</h2>
-	{!! Form::open() !!}
-	
-		<div class="col-md-6 form-group form-group--siginup">
+	<h2 class="login--welcome">Setting</h2>
+		{!! Form::open() !!}
+		
+			<div class="col-md-6 form-group form-group--siginup">
 
-			<div class="col-md-4 col-sm-offset-2 settings--label">
-				{!! Form::label('first_name', 'First Name:') !!}	
-			</div>
-
-			<div class="col-md-9 col-md-offset-2 signup--sm">
-				{!! Form::text('first_name', null, ['class' => 'col-md-12 form-control defult--input', 'placeholder' => 'First Name']) !!}
-			</div>
-		</div>
-
-		<div class="col-md-6 form-group form-group--siginup">
-			<div class="col-md-4 col-sm-offset-1 settings--label">
-				{!! Form::label('last_name', 'Last Name:') !!}
-			</div>
-
-			<div class="col-md-9 col-md-offset-1 signup--sm">
-				{!! Form::text('last_name', null, ['class' => 'col-md-12 form-control defult--input','placeholder' => 'Last Name']) !!}
-			</div>
-		</div>
-
-		<div class="col-md-6 form-group form-group--siginup">
-
-			<div class="col-md-4 col-sm-offset-2 settings--label">
-				{!! Form::label('email', 'Email:') !!}
-			</div>
-
-			<div class="col-md-9 col-md-offset-2 signup--sm">
-				{!! Form::email('email', null, ['class' => 'col-md-12 form-control defult--input','placeholder' => 'Email']) !!}
-			</div>
-
-		</div>
-
-		<div class="col-md-6 form-group form-group--siginup">
-
-			<div class="col-md-4 col-sm-offset-1 settings--label">
-				{!! Form::label('user_photo', 'Photo:') !!}
-			</div>
-
-			<div class="col-md-9 col-md-offset-1">
-				<div class="col-md-3">
-					{!! Form::file('email', null, ['class' => 'col-md-12 form-control defult--input','placeholder' => 'Email']) !!}
+				<div class="col-md-4 col-sm-offset-2 settings--label">
+					{!! Form::label('first_name', 'First Name:') !!}	
 				</div>
-				<div class="col-md-3 col-md-offset-5">
-					<img class="setting--upload-img" src="http://media.tumblr.com/tumblr_lnenqxmj0d1qiw26m.jpg" alt="">
+
+				<div class="col-md-9 col-md-offset-2 signup--md">
+					{!! Form::text('first_name', null, ['class' => 'col-md-12 form-control defult--input', 'placeholder' => 'First Name']) !!}
 				</div>
 			</div>
 
-		</div>
+			<div class="col-md-6 form-group form-group--siginup">
+				<div class="col-md-4 col-sm-offset-1 settings--label">
+					{!! Form::label('last_name', 'Last Name:') !!}
+				</div>
 
-		<div class="col-md-6 form-group form-group--siginup">
-
-			<div class="col-md-5 col-sm-offset-2 settings--label">
-				{!! Form::label('password', 'Create Password:') !!}
+				<div class="col-md-9 col-md-offset-1 signup--md">
+					{!! Form::text('last_name', null, ['class' => 'col-md-12 form-control defult--input','placeholder' => 'Last Name']) !!}
+				</div>
 			</div>
 
-			<div class="col-md-9 col-md-offset-2 signup--sm">
-				{!! Form::password('password', ['class' => 'col-md-12 form-control defult--input','placeholder' => 'Create Password']) !!}
+			<div class="col-md-6 form-group form-group--siginup">
+
+				<div class="col-md-4 col-sm-offset-2 settings--label">
+					{!! Form::label('email', 'Email:') !!}
+				</div>
+
+				<div class="col-md-9 col-md-offset-2 signup--md">
+					{!! Form::email('email', null, ['class' => 'col-md-12 form-control defult--input','placeholder' => 'Email']) !!}
+				</div>
+
 			</div>
 
-		</div>
+			<div class="col-md-6 form-group form-group--siginup">
 
-		<div class="col-md-6 form-group form-group--siginup">
+				<div class="col-md-4 col-sm-offset-1 settings--label">
+					{!! Form::label('user_photo', 'Photo:') !!}
+				</div>
 
-			<div class="col-md-5 col-sm-offset-2 settings--label">
-				{!! Form::label('password', 'Confirm Password:') !!}
+				<div class="col-md-9 col-md-offset-1">
+					<div class="col-md-3">
+						{!! Form::file('email', null, ['class' => 'col-md-12 form-control defult--input','placeholder' => 'Email']) !!}
+					</div>
+					<div class="col-md-3 col-md-offset-5">
+						<img class="setting--upload-img" src="http://media.tumblr.com/tumblr_lnenqxmj0d1qiw26m.jpg" alt="">
+					</div>
+				</div>
+
 			</div>
 
-			<div class="col-md-9 col-md-offset-2 signup--sm">
-				{!! Form::password('password', ['class' => 'col-md-12 form-control defult--input','placeholder' => 'Confirm Password']) !!}
+			<div class="col-md-6 form-group form-group--siginup">
+
+				<div class="col-md-5 col-sm-offset-2 settings--label">
+					{!! Form::label('password', 'Create Password:') !!}
+				</div>
+
+				<div class="col-md-9 col-md-offset-2 signup--md">
+					{!! Form::password('password', ['class' => 'col-md-12 form-control defult--input','placeholder' => 'Create Password']) !!}
+				</div>
+
 			</div>
 
-		</div>
+			<div class="col-md-6 form-group form-group--siginup">
 
-		<div class=" col-md-2 col-md-offset-10 form-button form-button--settings">
-			{!! Form::submit('Next', ['class' => 'btn btn-defult defult--input form-control']) !!}
-		</div>
-	{!! Form::close() !!}
+				<div class="col-md-5 col-sm-offset-2 settings--label">
+					{!! Form::label('password', 'Confirm Password:') !!}
+				</div>
+
+				<div class="col-md-9 col-md-offset-2 signup--md">
+					{!! Form::password('password', ['class' => 'col-md-12 form-control defult--input','placeholder' => 'Confirm Password']) !!}
+				</div>
+
+			</div>
+
+			<div class=" col-md-2 col-md-offset-10 form-button form-button--settings">
+				{!! Form::submit('Next', ['class' => 'btn btn-defult defult--input form-control']) !!}
+			</div>
+		{!! Form::close() !!}
 
 </div>
 
