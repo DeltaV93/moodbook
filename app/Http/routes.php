6ef134d17e257cbca4fddad0a12f725d7c86a388
Valerie Sharp <Valeriesharp93@gmail.com>
2016-03-30 19:51:25 -0700

    created dev branch, i think

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -53,7 +53,7 @@ Route::group(['middleware' => 'web'], function () {
     Route::auth();
 
     Route::get('/', function () {
-    	//dd(Auth::user());
+    	// dd(Auth::user());
 	    return view('index');
 	});
     
